24cd6755cd6c07c72f8384a81aef2f79177c1b42
Emil L <emillarsson99@gmail.com>
2026-05-28 16:23:39 +0200

    Created DI in Application layer

---
+++ UserProfileService.Application/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using UserProfileService.Application.Services;
+
+namespace UserProfileService.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<UserProfileAppService>();
+
+        return services;
+    }
+}
